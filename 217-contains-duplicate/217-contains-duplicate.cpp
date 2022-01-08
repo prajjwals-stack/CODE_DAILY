bed6caabecfaf7167464adc1e24ef18b371ccce1
Prajjwal soni <79002816+prajjwals-stack@users.noreply.github.com>
2022-01-08 14:27:20 +0530

    Time: 116 ms (21.64%), Space: 54.7 MB (6.96%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int>s;
+        for(int i=0;i<nums.size();i++){
+            s.insert(nums[i]);
+        }
+        return (s.size()==nums.size())?false:true;
+    }
+};
