3f39bd2c717bc72147cd0115e880661a21388aa5
Prajjwal soni <79002816+prajjwals-stack@users.noreply.github.com>
2022-04-11 18:30:54 +0530

    Time: 15 ms (5.60%), Space: 9.7 MB (72.66%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -4,13 +4,11 @@ public:
         int start=0;
         int end=nums.size()-1;
         while(start<=end){
-            int mid=start+(end-start)/2;
-            
-            if(nums[mid]==target)return mid;
-            
-            else if(nums[mid]>target)end=mid-1;
-            
-            else start=mid+1;
+            int mid=start +(end-start)/2;
+            if(nums[mid]==target) return mid;
+            else if (nums[mid]<target) start=mid+1;
+            else end=mid-1;
+        
         }
         return start;
     }
