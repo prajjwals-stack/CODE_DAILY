9c4b66d9e1d346fcaac84e075fd077e3b0abe919
Prajjwal soni <79002816+prajjwals-stack@users.noreply.github.com>
2022-06-21 15:54:09 +0530

    Time: 2 ms (67.26%), Space: 10.1 MB (44.53%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -12,16 +12,10 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p==NULL || q==NULL){
-            if(p==q){
-                return true;
-            }
-            else{
-                return false;
-            }
+        if(q==NULL || p==NULL){
+            if(p==q)return true;
+            else return false;
         }
-        return (p->val==q->val) && isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
-        
-       
+        return (p->val==q->val ) && isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
     }
 };
