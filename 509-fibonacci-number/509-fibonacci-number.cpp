33247cf4ece782d8b89afc716280b51ea6168907
Prajjwal soni <79002816+prajjwals-stack@users.noreply.github.com>
2022-08-18 21:06:20 +0530

    Time: 3 ms (59.15%), Space: 6 MB (39.80%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
+
     int fib(int n) {
-        if(n==0)return 0;
-        if(n==1)return 1;
+        int dp[n+2];
+        dp[0]=0;
+        dp[1]=1;
         
-        return fib(n-1)+fib(n-2);
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
