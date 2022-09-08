e3be875874190b014e9374128fb2d8b1c285adea
Prajjwal soni <79002816+prajjwals-stack@users.noreply.github.com>
2022-09-08 10:47:41 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (12.05%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -11,6 +11,13 @@
  */
 class Solution {
 public:
+//     if(root==NULL)return {};
+        
+//         inorderTraversal(root->left);
+//         v.push_back(root->val);
+//         inorderTraversal(root->right);
+        
+//         return v;
     vector<int>v;
     vector<int> inorderTraversal(TreeNode* root) {
         if(root==NULL)return {};
